a0c225a69c8906c9817c367bebe4a9ed1f60ae65
geoffrey <geoffrey.v.d@live.nl>
2020-11-10 21:29:20 +0100

    put it in a class, finalised tests

--- src/entity_linking.py
+++ src/entity_linking.py
@@ -3,56 +3,52 @@ import json
 from elasticsearch import Elasticsearch
 import trident
 
-KBPATH='./../assets/wikidata-20200203-truthy-uri-tridentdb'
-# Load the KB
-db = trident.Db(KBPATH)
-e = None
+class Entity_Linking:
+    def __init__(self, KBPATH):
+        self.e = Elasticsearch([{'host': 'localhost', 'port': 9200}],timeout=30)
+        self.db = trident.Db(KBPATH)
 
-def initialiseElastic():
-    global e
-    e=Elasticsearch([{'host': 'localhost', 'port': 9200}])
+    def searchElastic(self, query):    
+        p = {"query": {"query_string": {"query": query}}}
+        response = self.e.search(index="wikidata_en", body=json.dumps(p), size=10)
+        #idea maybe query name and a.k.a. instead of name and description (possibly faster more accurate since we often have the abbreviation)
+        id_labels = {}
+        if response:
+            for hit in response["hits"]["hits"]:
+                try:
+                    #same entity have schema name missing
+                    label = hit["_source"]["schema_name"]
+                except Exception as e:
+                    continue
+                id = hit["_id"]
+                #could also retrieve the ES score here
+                id_labels.setdefault(id, set()).add(label)
+        return id_labels
 
-def searchElastic(query):
-    global e
-    p = {"query": {"query_string": {"query": query}}}
-    response = e.search(index="wikidata_en", body=json.dumps(p))
-    #idea maybe query name and a.k.a. instead of name and description (possibly faster more accurate since we often have the abbreviation)
-    id_labels = {}
-    if response:
-        for hit in response["hits"]["hits"]:
-            try:
-                #same entity have schema name missing
-                label = hit["_source"]["schema_name"]
-            except Exception as e:
-                continue
-            id = hit["_id"]
-            #could also retrieve the ES score here
-            id_labels.setdefault(id, set()).add(label)
-    return id_labels
+    def searchTrident(self,query):
+        query="PREFIX wde: <http://www.wikidata.org/entity/> "\
+            "PREFIX wdp: <http://www.wikidata.org/prop/direct/> "\
+            "PREFIX wdpn: <http://www.wikidata.org/prop/direct-normalized/> "\
+            "select ?s where { ?s wdp:P31 wde:Q515 . } LIMIT 10"
+        results = db.sparql(query)
+        json_results = json.loads(results)
+        return json_results["results"]
 
-def searchTrident(query):
-    query="PREFIX wde: <http://www.wikidata.org/entity/> "\
-        "PREFIX wdp: <http://www.wikidata.org/prop/direct/> "\
-        "PREFIX wdpn: <http://www.wikidata.org/prop/direct-normalized/> "\
-        "select ?s where { ?s wdp:P31 wde:Q515 . } LIMIT 10"
-    results = db.sparql(query)
-    json_results = json.loads(results)
-    return json_results["results"]
-
-def entityLinking(entity_type):
-    entity_wikidata = [] #(entity, wikidata)
-    for entity in entity_type:
-        entitiy_popularity = [] #(entity, popularity)
-        #1look in elasticsearch for wikidate references per entity
-        for wikidata_url, label in searchElastic(entity[1]).items():
-            #for exampe now query trident and retreive the most popular (most references) wikipedia article for the entity
-            #MVP: use score and popularity
-            #2 retrieve info from trident
-            wikidata_ref = db.lookup_id(wikidata_url)
-            popularity = db.count_o(wikidata_ref)
-            entitiy_popularity.append((wikidata_url, popularity))
-            #TODO: better would be to use context dependent 
-        #3 identify the best possible match
-        entitiy_popularity.sort(key=lambda x: x[1], reverse=True)
-        entity_wikidata.append((entity[1],entitiy_popularity[0][0]))
-    return entity_wikidata
+    #!!a bit confusing but i use _ to anotate tuples, where the left side of _ means the first item in the tuple!!
+    def entityLinking(self, entitytype_entities):
+        entity_wikidata = [] #(entity, wikidata)
+        for entitytype_entity in entitytype_entities:
+            entitiy_popularity = [] #(entity, popularity)
+            #1look in elasticsearch for wikidate references per entity
+            for wikidata_url, label in self.searchElastic(entitytype_entity[1]).items():
+                #for exampe now query trident and retreive the most popular (most references) wikipedia article for the entity
+                #MVP: use score and popularity
+                #2 retrieve info from trident
+                wikidata_ref = self.db.lookup_id(wikidata_url)
+                popularity = self.db.count_o(wikidata_ref)
+                entitiy_popularity.append((wikidata_url, popularity)) #e.g. [('<http://www.wikidata.org/entity/Q271982>', 11)]
+                #TODO: better would be to use context dependent 
+            #3 identify the best possible match
+            entitiy_popularity.sort(key=lambda x: x[1], reverse=True)
+            entity_wikidata.append((entitytype_entity[1],entitiy_popularity[0][0]))
+        return entity_wikidata
--- test/linking_test.py
+++ test/linking_test.py
@@ -1,31 +1,34 @@
 import entity_linking as el
+from elasticsearch import Elasticsearch
+
+KBPATH='assets/wikidata-20200203-truthy-uri-tridentdb'
+entity_linking = el.Entity_Linking(KBPATH)
 
 #to run these tests elasticsearch should be running
-def initElastic():
-    initialiseElastic()
 
-def test_searchTrident():
-    assert 1 ==1
+# def test_searchTrident():
+#     assert 1 ==1
 
 def test_searchElastic():
-    entity = "ORGANIZATION", "Vrije Universiteit Amsterdam"
-    wikiurl_wikilabel = searchElastic(entity)
-    expected= [("<http://www.wikidata.org/entity/Q70764476>", "[The hospital of the \"Vrije Universiteit\" in Amsterdam]"),
-                ("<http://www.wikidata.org/entity/Q2610973>", "Hortus Botanicus Vrije Universiteit Amsterdam"),
-                ("<http://www.wikidata.org/entity/Q71001152>", "4.4. Starting New Data Conversations at Vrije Universiteit Amsterdam"),
-                ("<http://www.wikidata.org/entity/Q61671111>", "Faculteit der Rechtsgeleerdheid"),
-                ("<http://www.wikidata.org/entity/Q1065414>", "Vrije Universiteit"),
-                ("<http://www.wikidata.org/entity/Q44098890>", "Peter Mika"),
-                ("<http://www.wikidata.org/entity/Q65937341>", "Pia Sommerauer"),
-                ("<http://www.wikidata.org/entity/Q58894745>", "Victor de Boer"),
-                ("<http://www.wikidata.org/entity/Q2187167>", "Vrije Universiteit Amsterdam. Centrum voor Nederlandse Religiegeschiedenis"),
-                ("<http://www.wikidata.org/entity/Q612665>", "Vrije Universiteit Brussel")]
+    entity = ("ORGANIZATION", "Vrije Universiteit Amsterdam")
+    wikiurl_wikilabel = entity_linking.searchElastic(entity[1])
+    expected= {'<http://www.wikidata.org/entity/Q70764476>': {'[The hospital of the \\"Vrije Universiteit\\" in Amsterdam]'},
+     '<http://www.wikidata.org/entity/Q2610973>': {'Hortus Botanicus Vrije Universiteit Amsterdam'},
+     '<http://www.wikidata.org/entity/Q71001152>': {'4.4. Starting New Data Conversations at Vrije Universiteit Amsterdam'},
+     '<http://www.wikidata.org/entity/Q61671111>': {'Faculteit der Rechtsgeleerdheid'},
+     '<http://www.wikidata.org/entity/Q1065414>': {'Vrije Universiteit'},
+     '<http://www.wikidata.org/entity/Q44098890>': {'Peter Mika'},
+     '<http://www.wikidata.org/entity/Q65937341>': {'Pia Sommerauer'},
+     '<http://www.wikidata.org/entity/Q58894745>': {'Victor de Boer'},
+     '<http://www.wikidata.org/entity/Q2187167>': {'Vrije Universiteit Amsterdam. Centrum voor Nederlandse Religiegeschiedenis'},
+     '<http://www.wikidata.org/entity/Q612665>': {'Vrije Universiteit Brussel'}}
+    print(wikiurl_wikilabel)
     assert expected == wikiurl_wikilabel
 
 def test_entityLinking():
     entities = [
         ("ORGANIZATION", "Google")
     ]
-    entity_wikidata = entityLinking(entities)
+    entity_wikidata = entity_linking.entityLinking(entities)
     expected= [('Google', '<http://www.wikidata.org/entity/Q95>')]
     assert expected == entity_wikidata
