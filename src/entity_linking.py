24947df3b5082f2f09c6a1020fe238f747fdd24b
geoffrey <geoffrey.v.d@live.nl>
2020-11-10 22:25:13 +0100

    fixed type and removed trident query

--- src/entity_linking.py
+++ src/entity_linking.py
@@ -25,20 +25,11 @@ class Entity_Linking:
                 id_labels.setdefault(id, set()).add(label)
         return id_labels
 
-    def searchTrident(self,query):
-        query="PREFIX wde: <http://www.wikidata.org/entity/> "\
-            "PREFIX wdp: <http://www.wikidata.org/prop/direct/> "\
-            "PREFIX wdpn: <http://www.wikidata.org/prop/direct-normalized/> "\
-            "select ?s where { ?s wdp:P31 wde:Q515 . } LIMIT 10"
-        results = db.sparql(query)
-        json_results = json.loads(results)
-        return json_results["results"]
-
     #!!a bit confusing but i use _ to anotate tuples, where the left side of _ means the first item in the tuple!!
     def entityLinking(self, entitytype_entities):
         entity_wikidata = [] #(entity, wikidata)
         for entitytype_entity in entitytype_entities:
-            entitiy_popularity = [] #(entity, popularity)
+            entity_popularity = [] #(entity, popularity)
             #1look in elasticsearch for wikidate references per entity
             for wikidata_url, label in self.searchElastic(entitytype_entity[1]).items():
                 #for exampe now query trident and retreive the most popular (most references) wikipedia article for the entity
@@ -46,9 +37,9 @@ class Entity_Linking:
                 #2 retrieve info from trident
                 wikidata_ref = self.db.lookup_id(wikidata_url)
                 popularity = self.db.count_o(wikidata_ref)
-                entitiy_popularity.append((wikidata_url, popularity)) #e.g. [('<http://www.wikidata.org/entity/Q271982>', 11)]
+                entity_popularity.append((wikidata_url, popularity)) #e.g. [('<http://www.wikidata.org/entity/Q271982>', 11)]
                 #TODO: better would be to use context dependent 
             #3 identify the best possible match
-            entitiy_popularity.sort(key=lambda x: x[1], reverse=True)
-            entity_wikidata.append((entitytype_entity[1],entitiy_popularity[0][0]))
+            entity_popularity.sort(key=lambda x: x[1], reverse=True)
+            entity_wikidata.append((entitytype_entity[1],entity_popularity[0][0]))
         return entity_wikidata
